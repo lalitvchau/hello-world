67e8fcde1866d8c9492d1b2322968cc1021099ce
lalitk <lalit.kumar@ellucian.com>
2020-01-21 10:57:41 +0530

    Commit for intern demo

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
 	public static void main(String ...str){
 		System.out.println("Hello india................................gdf !");
+		System.out.println("Hello Interns !");
 	}
 }
